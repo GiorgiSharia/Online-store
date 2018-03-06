931653aec4425539b0889cd80c38a444e8d5c50d
Giorgi <gishar@ttu.ee>
2018-03-06 17:30:06 +0200

    Navigation bar created

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function mouseOver(id){
+    id.style.backgroundColor='#acb3bf';
+    id.style.cursor = 'pointer';
+    id.style.color='black';
+    
+}
+function mouseOff(id){
+    id.style.color='white';
+    id.style.backgroundColor = '#1f1f44';
+    
+}
+function register(){
+    console.log("Register");
+}
+function logIn(){
+    console.log("log in");
+}
