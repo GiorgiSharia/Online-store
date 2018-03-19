427c0b836243a5211d541ca719700cb1b83be82c
Giorgi <gishar@ttu.ee>
2018-03-19 20:51:40 +0200

    login and register boxes fade out added when clicked out of the boxes (might need improvement)

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
             $(this).css("background-color", "#1f1f44");
             $(this).css("color", "white");
     });
-
+//login box pops and background blurs
     $("#logButton").click(function(){
         $("#login").fadeIn();
         $("#login").siblings().css("opacity", "0.5");
     });
-
+//register box pops and background blurs
     $("#regButton").click(function(){
         $("#register").fadeIn();
         $("#register").siblings().css("opacity", "0.5");
@@ -41,4 +41,17 @@ $(document).ready(function(){
     $("#shoes").click(function(){
         $("#shoes_sub").toggle();
     });
+});
+//when clicked outside login and register boxes they fade out
+$('body').mouseup(function(e){
+    var subject = $("#register");
+    if(e.target.id == subject.attr('id')){
+        subject.fadeOut();
+        subject.siblings().css("opacity", "1");
+    }     
+    subject = $("#login");
+    if(e.target.id == subject.attr('id')){
+        subject.fadeOut();
+        subject.siblings().css("opacity", "1");
+    }     
 });
