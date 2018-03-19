83ffe20ab033f0dda53f78b2d9ee2da50fff1c7b
sojaph <jafaridze.sofo@gmail.com>
2018-03-19 19:34:53 +0200

    contact in bottom and opacity corrected

--- js/script.js
+++ js/script.js
@@ -10,47 +10,35 @@ $(document).ready(function(){
 
     $("#logButton").click(function(){
         $("#login").fadeIn();
-        $("#toHide").css('opacity','0.5');
+        $("#login").siblings().css("opacity", "0.5");
     });
 
     $("#regButton").click(function(){
         $("#register").fadeIn();
-        $("#toHide").css('opacity','0.5');
+        $("#register").siblings().css("opacity", "0.5");
     });
 
     $("#closeLog").click(function(){
         $('#login').css('display','none');
-        $("#toHide").css('opacity','1');
+        $("#login").siblings().css("opacity", "1");
     });
     $("#closeReg").click(function(){
         $('#register').css('display','none');
-        $("#toHide").css('opacity','1');
+        $("#register").siblings().css("opacity", "1");
     });
     $('#drop li').on('click', function() {
         $('#title').html($(this).find('a').html());
     });
     $("#electronics").click(function(){
-        $("#electronics_sub").css("display", "block");
-        $("#house_sub").css("display", "none");
-        $("#clothing_sub").css("display", "none");
-        $("#shoes_sub").css("display", "none");
+        $("#electronics_sub").toggle();
     });
     $("#clothing").click(function(){
-        $("#clothing_sub").css("display", "block");
-        $("#electronics_sub").css("display", "none");
-        $("#house_sub").css("display", "none");
-        $("#shoes_sub").css("display", "none");
+        $("#clothing_sub").toggle();
     });
     $("#house").click(function(){
-        $("#house_sub").css("display", "block");
-        $("#clothing_sub").css("display", "none");
-        $("#electronics_sub").css("display", "none");
-        $("#shoes_sub").css("display", "none");
+        $("#house_sub").toggle();
     });
     $("#shoes").click(function(){
-        $("#shoes_sub").css("display", "block");
-        $("#clothing_sub").css("display", "none");
-        $("#electronics_sub").css("display", "none");
-        $("#house_sub").css("display", "none");
+        $("#shoes_sub").toggle();
     });
 });
