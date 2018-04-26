97eb5f846ff72d0ba2edf38979abce80ca6b1220
Bidzasheni <george.qutateladze@gmail.com>
2018-04-27 01:01:06 +0300

    minor changes in changepsw

--- application/change_user_password.php
+++ application/change_user_password.php
@@ -3,22 +3,31 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-print_r($_SESSION);
 
 
 function changePSW()
 {
     //$password = $_POST['currPassword']; // required
     $newPassword = $_POST['newPassword']; // required
-    //$newPassword2 = $_POST['NewPassword2']; // required
+    $newPassword2 = $_POST['newPassword2']; // required
     $newPasswordH = password_hash($newPassword, PASSWORD_DEFAULT);
-    echo($newPassword);
+
+    if($newPassword != $newPassword2){
+        echo "New passwords don't match.";
+        echo nl2br("\n<a href='../ChangePSW.php'>Return to change password.</a>");
+        die();
+    }
 
     $id = $_SESSION['userID'];
-    echo($id);
     // create PDO connection object
     $dbConn = new DatabaseConnection();
     $pdo = $dbConn->getConnection();
+
+    /*$stmt = $pdo->prepare("SELECT password FROM `users` WHERE id = :id LIMIT 1");
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $res = $stmt->get_result();
+    echo($res);*/
 
     try {
         $statement = $pdo->prepare("SELECT * FROM `users` WHERE id = :id LIMIT 1");
@@ -26,7 +35,7 @@ function changePSW()
         $statement->execute();
 
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        echo "SADASDASD";
+
 
         // no user matching the email
         if (empty($result)) {
@@ -38,17 +47,12 @@ function changePSW()
 
         // Prepare statement
         $stmt = $pdo->prepare($sql);
-        echo "AFGHANIKO";
         // execute the query
         $update_status = $stmt->execute(array(':newPasswordH' => $newPasswordH, ':id' => $id));
-        echo "IHAAA";
-        echo($update_status);
+
         if ($update_status === TRUE) {
             echo("Record updated successfully" . "\r\n");
-            echo nl2br("\nPassword: ");
-            echo ($newPassword);
-            echo nl2br("\nHashed Password: ");
-            echo ($newPasswordH);
+            echo nl2br("\n<a href='../Homepage.php'>Return to homepage</a>");
             return true;
         } else {
             echo "Error updating record";
@@ -70,7 +74,6 @@ if($_SESSION['isLoggedIn'] == true) {
 
     unset($_SESSION['success_message']);
     unset($_SESSION['error_message']);
-
     changePSW();
 
 }
