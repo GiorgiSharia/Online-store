69f2d388601bac873b82ef6afb195d6b39de74ec
unknown <george.qutateladze@gmail.com>
2018-05-10 22:21:23 +0300

    armaxsovs

--- application/Filter.php
+++ application/Filter.php
@@ -10,7 +10,7 @@ class Filter {
         $dbConn = new DatabaseConnection();
         $pdo = $dbConn->getConnection();
 
-        $productCategory=$_GET["category"];
+        $productCategory = $_GET["category"];
 
         if($productCategory == "Phones" || $productCategory == "Cameras" || $productCategory == "SmartWatches" || $productCategory == "Accessories"){
             $statement = $pdo->prepare("SELECT * FROM `products` WHERE category=:category");
--- pay.php
+++ pay.php
@@ -34,11 +34,13 @@ AAD/4AAA//AAAA==" rel="icon" type="image/x-icon" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- fa fa symobls -->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/script.js"></script>
 <link rel="stylesheet" href="css/css1.css">
 <title>eShop</title>
-<script src="js/script.js"></script>
+
 </head>
 
 <div id="includeContent"></div>
@@ -91,7 +93,3 @@ AAD/4AAA//AAAA==" rel="icon" type="image/x-icon" />
 </div>
 </html>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
-<script src="js/script.js"></script>
