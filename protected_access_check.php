6fb26c4e359ffd74baeccbe1d03e6a05ceba0cf5
Giorgi <gishar@ttu.ee>
2018-05-10 22:44:23 +0300

    not logged in trying to buy developed

--- protected_access_check.php
+++ protected_access_check.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
     if ($_SESSION['isLoggedIn'] !== true) {
-        echo "<script type='text/javascript'>alert('You must be logged in to shop');</script>";
-        $_SESSION['error_message'] = 'You must be logged in!';
-        header('Location: userForm.php');
+        echo "<script type='text/javascript'>var answer = confirm('You must be logged in to shop');if(answer){window.location.href = 'userForm.php';}</script>";
     }
 ?>
